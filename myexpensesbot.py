0ac3222869f7f3329cc74787987ebcf05c4ae39d
pmuriuki <pmuriuki@alba.one>
2019-12-07 19:45:40 +0300

    Update project structure

--- myexpensesbot.py
+++ myexpensesbot.py
@@ -1,5 +1,5 @@
-##!/usr/bin/env python3
-#!/usr/bin/python3
+#!/usr/bin/env python3
+##!/usr/bin/python3
 """
 @ author: PMuriuki
 """
@@ -8,9 +8,14 @@ from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler, ConversationHandler, Filters
 
 from libs.utils import logger
-from bot import handlers
+from bot.handlers import auth, newexpense, expensestats, budgetlimits
+# from bot.handlers.newexpense import *
+# from bot.handlers.expensestats import *
+# from bot.handlers.budgetlimits import *
 from bot.globals import CHOOSING, TYPING_REPLY
 
+# TODO: Create dev env on mongo, separate from prod
+# TODO: REname "limits" to "budget limits"
 # TODO: Add pipeline to accept proof as images and post to API
 # TODO: Improve expenses report conversation flow
 # TODO: Add step to end session/after period of inactivity
@@ -27,9 +32,9 @@ def main():
     # Start and verify
     # TODO: add a "sorry, that's not a valid choice" fallback
     dispatcher.add_handler(ConversationHandler(
-        entry_points=[CommandHandler('start', handlers.start)],
+        entry_points=[CommandHandler('start', auth.start)],
         states={
-            TYPING_REPLY: [MessageHandler(Filters.text, handlers.verify)],                       
+            TYPING_REPLY: [MessageHandler(Filters.text, auth.verify)],                       
                 },
         fallbacks=[]
         ))
@@ -37,20 +42,20 @@ def main():
     # Enter new expense
     # TODO: add a "sorry, that's not a valid choice" fallback
     dispatcher.add_handler(ConversationHandler(
-        entry_points=[MessageHandler(Filters.regex('New Expense'), handlers.newExpense)],
+        entry_points=[MessageHandler(Filters.regex('New Expense'), newexpense.newExpense)],
         states = {
-            CHOOSING : [MessageHandler(Filters.regex('Timestamp'), handlers.timestamp),
-                        MessageHandler(Filters.regex('Description'), handlers.description),
-                        MessageHandler(Filters.regex('Category'), handlers.category),
-                        MessageHandler(Filters.regex('Proof'), handlers.proof),
-                        MessageHandler(Filters.regex('Amount'), handlers.amount),
-                        MessageHandler(Filters.regex('Submit'), handlers.postExpense),
-                        MessageHandler(Filters.regex('Abort'), handlers.home)],
-            TYPING_REPLY: [ MessageHandler(Filters.text, handlers.verifyValue),
-                            CommandHandler('home', handlers.home),
-                            CommandHandler('cancel', handlers.newExpense),
-                            CommandHandler('done', handlers.nextExpenseField),
-                            CommandHandler('submit', handlers.postExpense)],
+            CHOOSING : [MessageHandler(Filters.regex('Timestamp'), newexpense.timestamp),
+                        MessageHandler(Filters.regex('Description'), newexpense.description),
+                        MessageHandler(Filters.regex('Category'), newexpense.category),
+                        MessageHandler(Filters.regex('Proof'), newexpense.proof),
+                        MessageHandler(Filters.regex('Amount'), newexpense.amount),
+                        MessageHandler(Filters.regex('Submit'), newexpense.postExpense),
+                        MessageHandler(Filters.regex('Abort'), auth.home)],
+            TYPING_REPLY: [ MessageHandler(Filters.text, newexpense.verifyValue),
+                            CommandHandler('home', auth.home),
+                            CommandHandler('cancel', newexpense.newExpense),
+                            CommandHandler('done', newexpense.nextExpenseField),
+                            CommandHandler('submit', newexpense.postExpense)],
             },
         fallbacks=[]
         ))
@@ -59,34 +64,33 @@ def main():
     # TODO: add a "sorry, that's not a valid choice" fallback
     # TODO: separate Limits, expenses. limits->view,update,set | expenses->by month, by category
     dispatcher.add_handler(ConversationHandler(
-        entry_points=[MessageHandler(Filters.regex('Expenses Report'), handlers.expensesReport)],
+        entry_points=[MessageHandler(Filters.regex('Expenses Report'), expensestats.expensesReport)],
         states = {
-            CHOOSING : [MessageHandler(Filters.regex('Abort'), handlers.home),
-                        MessageHandler(Filters.regex('Set Limits'), handlers.setLimits),
-                        MessageHandler(Filters.regex('Update Limits'), handlers.updateLimitsWRVW),
-                        MessageHandler(Filters.regex('View Limits'), handlers.viewLimits),
-                        MessageHandler(Filters.regex('View Expenses By Month'), handlers.totalByMonth),
-                        MessageHandler(Filters.regex('View Expenses By Category'), handlers.totalByCategory),
-                        CommandHandler('update', handlers.updateLimitsnoRVW),
-                        CommandHandler('home', handlers.home),
-                        CommandHandler('cancel', handlers.expensesReport),
-                        # MessageHandler(Filters.text, handlers.limitKey),
-                        CommandHandler('done', handlers.reviewLimits)],
-            TYPING_REPLY : [MessageHandler(Filters.regex('(^Set limit for )'), handlers.limitKey),
-                            MessageHandler(Filters.regex('^[0-9]'), handlers.limitValue),
-                            # MessageHandler(Filters.regex('^[a-zA-Z]'), handlers.selectMonth),
-                            MessageHandler(Filters.regex('(?:Jan$|Feb$|Mar$|Apr$|May$|June$|July$|Aug$|Sept$|Oct$|Nov$|Dec$)'), handlers.selectMonth),
-                            MessageHandler(Filters.regex('(^Total expenses for )'), handlers.selectCategory),
-                            CommandHandler('submit', handlers.postLimits),
-                            CommandHandler('edit', handlers.setLimits),
-                            CommandHandler('cancel', handlers.expensesReport),
-                            CommandHandler('home', handlers.home)],
+            CHOOSING : [MessageHandler(Filters.regex('Abort'), auth.home),
+                        MessageHandler(Filters.regex('Set Limits'), budgetlimits.setLimits),
+                        MessageHandler(Filters.regex('Update Limits'), budgetlimits.updateLimitsWRVW),
+                        MessageHandler(Filters.regex('View Limits'), budgetlimits.viewLimits),
+                        MessageHandler(Filters.regex('View Expenses By Month'), expensestats.totalByMonth),
+                        MessageHandler(Filters.regex('View Expenses By Category'), expensestats.totalByCategory),
+                        CommandHandler('update', budgetlimits.updateLimitsnoRVW),
+                        CommandHandler('home', auth.home),
+                        CommandHandler('cancel', expensestats.expensesReport)],
+            TYPING_REPLY : [MessageHandler(Filters.regex('(^Set limit for )'), budgetlimits.limitKey),
+                            MessageHandler(Filters.regex('^[0-9]'), budgetlimits.limitValue),
+                            CommandHandler('done', budgetlimits.reviewLimits),
+                            # MessageHandler(Filters.regex('^[a-zA-Z]'), selectMonth),
+                            MessageHandler(Filters.regex('(?:Jan$|Feb$|Mar$|Apr$|May$|June$|July$|Aug$|Sept$|Oct$|Nov$|Dec$)'), expensestats.selectMonth),
+                            MessageHandler(Filters.regex('(^Total expenses for )'), expensestats.selectCategory),
+                            CommandHandler('submit', budgetlimits.postLimits),
+                            CommandHandler('edit', budgetlimits.setLimits),
+                            CommandHandler('cancel', expensestats.expensesReport),
+                            CommandHandler('home', auth.home)],
         },
         fallbacks=[]
     ))
     
     # log errors
-    dispatcher.add_error_handler(handlers.error)
+    dispatcher.add_error_handler(auth.error)
 
     # Start the bot
     updater.start_polling()
